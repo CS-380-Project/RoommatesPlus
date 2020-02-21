26d6db4fdb62f2e7b45749dc4d599ff6555f83b6
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2020-02-20 19:51:43 -0800

    Hotfix For Crashes
    Updated:
       -[Login.js] (got rid of double call to
       navigation causing crash and cleaned
       up the render method)

--- RoommatesPlus/frontend/screens/Login.js
+++ RoommatesPlus/frontend/screens/Login.js
@@ -4,6 +4,7 @@ import { Text, View, TextInput,StyleSheet,Dimensions, TouchableOpacity,Image} fr
 import Fire from '../../backend/Fire';
 import firebase from 'firebase';
 import { Alert } from 'react-native';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 
 const {width,height} = Dimensions.get('window')
@@ -28,47 +29,40 @@ export default class Login extends Component {
   // front end render input fields and button
   render(){
     return (
-    
       <View style={{flex:1, backgroundColor: 'white', justifyContent:'flex-end'}}>
         <View style={{...StyleSheet.absoluteFill}}>
-        <Image source={{ uri: "https://firebasestorage.googleapis.com/v0/b/roommatesplus-15f85.appspot.com/o/backimage.jpg?alt=media&token=485aedbd-8de0-46ce-bc0d-a2dc9071f435" }}
-          style={{flex:1}}
-          />
+          <Image source={{ uri: "https://firebasestorage.googleapis.com/v0/b/roommatesplus-15f85.appspot.com/o/backimage.jpg?alt=media&token=485aedbd-8de0-46ce-bc0d-a2dc9071f435" }}
+            style={{flex:1}}/>
         </View>
         <View style={{justifyContent: 'center', height: 400 }}>
 
         <Text style={styles.headline}>Email</Text>
 
-<TextInput style = {styles.textInput}  placeholder="Enter your email"
-    onChangeText={(email) => this.setState({email})}
-    value={this.state.email}/>
+        <TextInput style = {styles.textInput}  placeholder="Enter your email"
+            onChangeText={(email) => this.setState({email})}
+            value={this.state.email}/>
 
-<Text style={styles.headline}>Password</Text>
+        <Text style={styles.headline}>Password</Text>
 
-<TextInput  secureTextEntry style = {styles.textInput} placeholder =  "enter password"  onChangeText={(password) => this.setState({password})}
-    value={this.state.password}/>
+        <TextInput  secureTextEntry style = {styles.textInput} placeholder =  "enter password"  
+            onChangeText={(password) => this.setState({password})}
+            value={this.state.password}/>
 
+        <TouchableOpacity onPress = {this.onLoginButtonPress}
+          style = {styles.button}>
+          <Text style={styles.buttonText}>SIGN IN</Text>
+        </TouchableOpacity>
 
-<TouchableOpacity onPress = {() => {this.onLoginButtonPress(); this.props.navigation.navigate('Roommates');}}
-  style = {styles.button}>
-  <Text style={styles.buttonText}>SIGN IN</Text>
-</TouchableOpacity>
+        <TouchableOpacity onPress = {() => this.props.navigation.navigate('CrtAccountScreen')}
+          style = {styles.button}>
+          <Text style={styles.buttonText}>CREATE ACCOUNT</Text>
+        </TouchableOpacity>
 
-<TouchableOpacity onPress = {() => this.props.navigation.navigate('CrtAccountScreen')}
-  style = {styles.button}>
-  <Text style={styles.buttonText}>CREATE ACCOUNT</Text>
-</TouchableOpacity>
-
-<TouchableOpacity onPress = {() => this.props.navigation.navigate('Roommates')}
-  style = {styles.button}>
-  <Text style={styles.buttonText}>GO TO DASHBOARD</Text>
-</TouchableOpacity> 
-
-        </View>
-
-
-      
-             
+        <TouchableOpacity onPress = {() => this.props.navigation.navigate('Roommates')}
+          style = {styles.button}>
+          <Text style={styles.buttonText}>GO TO DASHBOARD</Text>
+        </TouchableOpacity> 
+        </View>             
       </View>
     );
     }
