be6cbd053e1c8ad3d7ff83e166fc2bfbbf6e5a36
mvaldivia99 <michellvaldivia1@live.com>
2020-03-02 10:34:04 -0800

    Update JoinHousehold - updated JoineHousehold.js - updated Navigation.js to link to JoineHousehold instead of   CreateHousehold

--- RoommatesPlus/frontend/screens/JoinHouseHold.js
+++ RoommatesPlus/frontend/screens/JoinHouseHold.js
@@ -1,37 +1,104 @@
 import React, { Component } from 'react';
 import { styles } from '../styles/style';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, Alert } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import firebase from 'firebase';
+import Fire from '../../backend/Fire';
 
-export default class CreateHouseHold extends Component {
+export default class JoinHouseHold extends Component {
     state = {
         houseSearch: '',
         houseFound: '',
+        houseID: '',
+        usersExist: false,
     }
     
+    joinHousehold = () =>{
+        let usersExist = false, data = {};
+        let thisUser = Fire.shared.udi.uid;
+        // check if the users field exists
+        firebase.firestore().collection('households').doc(this.state.houseID).get()
+        .then(doc =>{
+            // should not run; already checked if household exists in getHouseholdInfo()
+            if (!doc.exists){ 
+                console.log("Household not found.");
+                return;
+            } else { 
+                if (doc.data().members[0] != " "){
+                    usersExist = true;
+                    console.log("Ran this fucking block: " + usersExist);
+                } else {
+                    console.log("There are no users.");
+                }
+
+                console.log("2 time: " + usersExist);
+            }  
+        })
+        .catch(err => {
+            console.log("Error: ", err)
+        });
+
+        console.log("3 time: " + usersExist);
+
+        if (usersExist){
+            console.log("ran if");
+            // users list already exists; add this user to it
+            firebase.firestore().collection('households').doc(this.state.houseID).get()
+            .then(doc =>{
+                doc.update({
+                    members: arrayUnion(thisUser)
+                });
+            })
+            .catch(err => {
+                console.log("Error: ", err)
+            });
+        } else {
+            console.log("ran else");
+            // create a new users field and set it with the user email
+            firebase.firestore().collection('households').doc(this.state.houseID).set({
+                "members": [thisUser],
+            }, {merge: true});
+        }
+
+    }
+
     getHouseholdInfo = () => {
         console.log(this.state.houseSearch);
-        let houseHolds = firebase.firestore().collection('households');
+        let houseHolds = firebase.firestore().collection("households");
         let query = houseHolds.where('nickname', '==', this.state.houseSearch).get()
         .then(snapshot => {
-            console.log("ran snapshot");
             if (snapshot.empty){
-                console.log('Household not found');
+                Alert.alert(
+                    'Household Not Found',
+                    this.state.houseSearch + 'not found. Please try again.',     
+                    [
+                        { text: "cancel", onPress: () => console.log("cancel Pressed") }
+                    ],                   
+                    { cancelable: true},
+                );
                 return;
             }
 
             snapshot.forEach(doc => {
-                console.log(doc.id, '=>', doc.data());
                 this.setState({
+                    houseID: doc.id,
                     houseFound: doc.data().nickname, 
                 })
-                           
             })
+            Alert.alert(
+                'Household Found!',
+                this.state.houseFound + ' found. Would you like to join the household?.',     
+                [
+                    { text: "Yes", onPress: this.joinHousehold },
+                    { text: "No", onPress: () => console.log("No Pressed")}
+                ],                   
+                { cancelable: true},
+            );
+            return;
         })
         .catch(err => {
             console.log("Error getting household: ", err);
-        });       
+        });     
     }
 
     render() {
@@ -42,10 +109,9 @@ export default class CreateHouseHold extends Component {
                 value = {this.state.houseSearch}
                 onChangeText={(houseSearch) => this.setState({houseSearch})}/>                 
                 
-                <TouchableOpacity style = { styles.button } onPress = {this.getHouseholdInfo}>
+                <TouchableOpacity style = { styles.button } onPress = {this.getHouseholdInfo.bind()}>
                     <Text style = { styles.buttonText }>Search</Text>
                 </TouchableOpacity>
-                <Text>{this.state.houseFound}</Text>
             </View>
         );
     }
