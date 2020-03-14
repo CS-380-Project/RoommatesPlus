9e4454c386f335644a209d252075666b36e022a1
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2020-03-14 10:05:34 -0700

    Revert "Revert "Merge pull request #17 from CS-380-Project/Chores""
    This reverts commit 9b26e279931cd874d2e5dc882f6120fe8e779da6.

--- RoommatesPlus/frontend/screens/Messaging.js
+++ RoommatesPlus/frontend/screens/Messaging.js
@@ -1,54 +1,22 @@
 import React, { Component } from 'react';
-//import { styles } from '../styles/style';
-import {View, StyleSheet, TextInput, Text, TouchableOpacity} from 'react-native';
+import { styles } from '../styles/style';
+import { Text, View, TextInput } from 'react-native';
 import {Header, Left, Icon} from 'native-base';
-import ChatScreen from './ChatScreen';
 
-class Messaging extends Component {
-    state = { name: '' }
-    onPress = () => {
-        // 1.
-        this.props.navigation.navigate("ChatScreen", {name: this.state.name})
-    }
-    
-    onChangeText = name => this.setState({ name });
-    
+export default class Messaging extends Component {
+
     render() {
         return (
-          <View>
-              <Text style={styles.title}>Enter your name:</Text>
-            <TextInput
-            onChangeText={this.onChangeText} // <- Add this
-            style={styles.nameInput}
-            placeHolder="House ID"
-            value={this.state.name}
-                    />
-        <TouchableOpacity onPress={this.onPress}>
-          <Text style={styles.buttonText}>Next</Text>
-        </TouchableOpacity>
-          </View>
+            <View style = {{flex: 1}}>
+                <Header style = {styles.header}>
+                    <Left style = {{flex: 1, marginHorizontal: 10}}>
+                    <Icon name={'menu'} style={{color: 'black'}} onPress={() => this.props.navigation.openDrawer()}/>
+                    </Left>
+                </Header>
+                <View style = {styles.container}>
+                    <Text>Messaging Screen</Text>
+                </View>
+            </View>
         );
-      }
     }
-
-    const offset = 24;
-    const styles = StyleSheet.create({
-      nameInput: { // 3. <- Add a style for the input
-        height: offset * 2,
-        margin: offset,
-        paddingHorizontal: offset,
-        borderColor: '#111111',
-        borderWidth: 1,
-      },
-      title: { // 4.
-        marginTop: offset,
-        marginLeft: offset,
-        fontSize: offset,
-      },
-      buttonText: { // 5.
-        marginLeft: offset,
-        fontSize: offset,
-      },
-    });
-
-export default Messaging;
+}
